5251039ba0ac9d13c6f7a583928d4e57e60630db
양기현 <rlgusdid@naver.com>
2021-04-28 19:37:28 +0900

    [baekjoon] 수 찾기

---
+++ baekjoon/1920.py
@@ -0,0 +1,11 @@
+# 수 찾기
+n = int(input())
+arr = set(map(int, input().split()))
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+    if i not in arr:
+        print('0')
+    else:
+        print('1')
