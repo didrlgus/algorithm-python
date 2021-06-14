6b328a934bf73f0288e8b9fea7bd34d009c6bdee
양기현 <rlgusdid@naver.com>
2021-06-14 20:01:01 +0900

    [baekjoon] 수 찾기

--- baekjoon/1920.py
+++ baekjoon/1920.py
@@ -1,11 +1,16 @@
 # 수 찾기
-n = int(input())
-arr = set(map(int, input().split()))
-m = int(input())
-x = list(map(int, input().split()))
+N, A = int(input()), {i : 1 for i in list(map(int, input().split()))}
+M = int(input())
+for i in list(map(int, input().split())):
+    print(A.get(i, 0))
 
-for i in x:
-    if i not in arr:
-        print('0')
-    else:
-        print('1')
+# n = int(input())
+# arr = set(map(int, input().split()))
+# m = int(input())
+# x = list(map(int, input().split()))
+
+# for i in x:
+#     if i not in arr:
+#         print('0')
+#     else:
+#         print('1')
